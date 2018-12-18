c88ff3c8dd79e3fefcbe8277ec1a2a90d6293882
Ruchi Aggarwal <ruchi.aggarwal@river-island.com>
2018-12-18 14:17:49 +0000

    Add files via upload

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	)
+
+func main() {
+	fmt.Println("go","lang")
+	fmt.Println("4/3 = ",4/3)
+	fmt.Println(true && true )
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
